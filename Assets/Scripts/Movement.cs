52f43516628e4d53564829c4bc20453fd7ebd5a9
Camilo Villegas <camilovillegas@Camilos-MacBook-Pro.local>
2021-01-30 02:02:36 -0500

    Added basic player movement, water shader and physics to keep player afloat

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody rigid;
+    public float horizontal;
+    public float vertical;
+    public float rate = 100f;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0.0f, vertical);
+        rigid.AddForce(direction * rate);
+    }
+}
