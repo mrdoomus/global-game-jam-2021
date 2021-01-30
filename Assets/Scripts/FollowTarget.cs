ca5acfbc8f14886d5cb12a5225d0ae9e9601a580
Camilo Villegas <camilovillegas@Camilos-MacBook-Pro.local>
2021-01-30 11:32:52 -0500

    Basic children following player bhaviour

---
+++ Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    public Transform objectToFollow;
+    public Vector3 offset;
+    public bool hasCollided = false;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player") hasCollided = true;
+    }
+
+    void Update()
+    {
+        if(hasCollided) transform.position = objectToFollow.position + offset;
+    }
+}
